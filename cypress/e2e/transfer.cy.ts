6e9b2ec9cf23912d122bef2650735defb8bab017
SimoAcharouaou777 <acharouaoumohamed@gmail.com>
2025-01-15 10:03:33 +0100

    tests

--- cypress/e2e/transfer.cy.ts
+++ cypress/e2e/transfer.cy.ts
@@ -1,5 +1,13 @@
 describe('Transfer Page', () => {
   beforeEach(() => {
+    cy.intercept('GET', '**/api/user/accounts*',  {
+      statusCode: 200,
+      body: [
+        {id: 1, accountType: 'Checking', accountNumber: '123', balance: 1000},
+        {id: 2, accountType: 'Savings', accountNumber: '456', balance: 2000}
+      ],
+    }).as('getAccounts');
+
     cy.visit('http://localhost:4200/login');
     cy.get('input[name="username"]').type('simo');
     cy.get('input[name="password"]').type('password');
@@ -7,6 +15,7 @@ describe('Transfer Page', () => {
 
     cy.url().should('not.include', '/login');
     cy.visit('http://localhost:4200/user/transfer');
+    cy.wait('@getAccounts');
   });
 
   it('should display the page title', () => {
@@ -20,16 +29,14 @@ describe('Transfer Page', () => {
   }) ;
 
   it('should display the deposit form and perform a deposit', () => {
-    cy.get('form[formGroup="depositForm"]').should('exist');
-    cy.get('form[formGroup="depositForm"] select[formControlName="accountId"]')
-      .should('be.visible')
-      .select('1');
+    cy.get('form[formGroup="depositForm"]', { timeout: 10000 }).should('exist');
+    cy.get('form[formGroup="depositForm"] select[formControlName="accountId"] option')
+      .should('have.length.greaterThan', 1);
 
+    cy.get('form[formGroup="depositForm"] select[formControlName="accountId"]').select('1');
     cy.get('form[formGroup="depositForm"] input[formControlName="amount"]').type('100');
     cy.get('form[formGroup="depositForm"] button[type="submit"]').click();
     cy.contains('Deposit successful.').should('exist');
   });
-
-
 
 })
